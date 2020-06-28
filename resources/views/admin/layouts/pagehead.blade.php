5e188d85ac4c135b02ea41bd034f336311589443
Edsonmoses <edsonwandera@yahoo.com>
2020-06-28 20:08:29 +0300

    Delete all the stuff

--- resources/views/admin/layouts/pagehead.blade.php
+++
@@ -1,4 +0,0 @@
-<h1>
-  Frame54 Page
-  <small>Lets create something awesome</small>
-</h1>
--- resources/views/user/download.blade.php
+++
@@ -1,5 +0,0 @@
-<h1>Doenload</h1>
-<form action="" method="GET">
-    <input type="text" name="category" required/>
-    <button type="submit">Submit</button>
-</form>
