a00b426d7d970eda39e5fcb75d755b4d5f4993b2
Edsonmoses <edsonwandera@yahoo.com>
2020-08-01 12:37:16 +0300

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         /**if (env('APP_ENV') === 'production') {
-         *   \Illuminate\Support\Facades\URL::forceScheme('https');
+           \Illuminate\Support\Facades\URL::forceScheme('https');
         }*/
-        $this->app['request']->server->set('HTTPS','on');
+
+        //$this->app['request']->server->set('HTTPS','on');
     }
 }
