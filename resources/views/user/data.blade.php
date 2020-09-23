40ab0562d8662e26c655185e593b0506098060d4
Edsonmoses <edsonwandera@yahoo.com>
2020-09-23 11:01:35 +0300

    similer image link

--- resources/views/user/data.blade.php
+++ resources/views/user/data.blade.php
@@ -141,9 +141,9 @@
              <div class="similar">
             <p> SIMILAR PHOTOS</p>
             <div class="row">
-                <div class="col-lg-6 col-md-6">
+                <div class="col-lg-6 col-md-6 gal">
             @foreach ($posts->categories as $category)
-              <div class="frame  gal">
+              <div class="frame">
              <a href="{{ $post->slug }}">
                  <div class="ftitle"><img alt="{{ $posts->name ? $posts->name : $posts->title }}" src="/uploads/avatars/{{$posts->avatar}}" class="ftitle-img  img-responsive" width="50" height="50"> <span>{{$posts->name? $posts->name : 'Frame54'}}</span></div>
                  <img src= "{{ Storage::url($posts->image)}}" alt="{{$posts->title}}" class="img-sm">
